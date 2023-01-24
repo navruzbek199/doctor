821ff16964a3d0295968aac343185a338d171736
navruz <zubaydullayevnavruz@gmail.com>
2023-01-23 18:02:31 -0800

    server

--- src/redux/apiRoot/apiRoot.js
+++ src/redux/apiRoot/apiRoot.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const apiRoot = axios.create({
-  baseURL: `http://192.168.100.225:8082/`
+  baseURL: `http://ec2-43-206-161-114.ap-northeast-1.compute.amazonaws.com:8083/`
 })
 export default apiRoot
